06e989236a3b088d7879095b1d1a8b00c458bcf6
praneeth003 <ch.krishnapraneeth@gmail.com>
2025-02-18 22:02:12 -0500

    initial commit

---
+++ frontend/public/content.js
@@ -0,0 +1,7 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "getPageContent") {
+    const content = document.body.innerText;
+    sendResponse({ content });
+  }
+  return true;
+});
